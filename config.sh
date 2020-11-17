a7eede58e76999ed8c388f4ee6eb4cf8800ff74a
god <god@pop-os.localdomain>
2020-11-17 14:52:10 +0100

    Improve theme config

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Set up configs"
-./script/fira_code.sh
+./script/font.sh
 mkdir --parent --verbose ~/.local/share/wallpaper
 cat ./wallpaper/RobinBird.jpg > ~/.local/share/wallpaper/RobinBird.jpg
 cat ./config/profile > ~/.profile
@@ -11,4 +11,6 @@ cat ./config/status.toml > ~/.config/i3/status.toml
 mkdir -p -v ~/.config/alacritty
 cat ./config/alacritty.yml > ~/.config/alacritty.yml
 cat ./config/regolith > ~/.config/regolith/Xresources
+sudo cp -r ./config/gruvbox-material /etc/regolith/styles
+regolith-look set gruvbox-material
 regolith-look refresh
--- script/font.sh
+++ script/font.sh
@@ -13,6 +13,16 @@ for type in Bold Light Medium Regular Retina; do
 	    echo "Found existing file $file_path"
     fi;
 done
+file_path="${HOME}/.local/share/fonts/materialdesignicons-webfont.ttf"
+file_url="https://gist.github.com/draoncc/3c20d8d4262892ccd2e227eefeafa8ef/raw/3e6e12c213fba1ec28aaa26430c3606874754c30/MaterialIcons-Regular-for-inline.ttf"
+if [ ! -e "${file_path}" ]; then
+    echo "wget -O $file_path $file_url"
+    wget -q -O "${file_path}" "${file_url}"
+    CHANGE=true
+else
+echo "Found existing file $file_path"
+fi;
+
 
 if ( $CHANGE ) then
     echo "Refresh font cache"
