aceb72d24de8b9e40ef62d120520dfb5e08ab8a0
Your Name <you@example.com>
2020-11-23 20:38:19 +0100

    Add bat and lf config

--- link.sh
+++ link.sh
@@ -8,15 +8,25 @@ cat ./config/profile > ~/.profile
 
 echo "Link wallpaper"
 mkdir --parent --verbose ~/.local/share/wallpaper
-ln -v -f ./wallpaper/RobinBird.jpg ~/.background-image
+ln -f ./wallpaper/RobinBird.jpg ~/.background-image
 
 echo "Link alacritty config"
 mkdir --parent --verbose ~/.config
-ln -v -f ./config/alacritty.yml ~/.config/alacritty.yml
+ln -f ./config/alacritty.yml ~/.config/alacritty.yml
+
+echo "Link lf config"
+mkdir --parent --verbose ~/.config/lf
+ln -f ./config/lfrc ~/.config/lf/lfrc
+
+echo "Link bat config"
+mkdir --parent --verbose ~/.config/bat/themes
+ln -f ./config/bat ~/.config/bat/config
+ln -f ./config/gruvbox.tmTheme ~/.config/bat/themes/gruvbox.tmTheme
+bat cache --build
 
 echo "Link i3 config"
-ln -v -f ./config/i3/status.toml ~/.config/i3/status.toml
-ln -v -f ./config/i3/config ~/.config/i3/config
+ln -f ./config/i3/status.toml ~/.config/i3/status.toml
+ln -f ./config/i3/config ~/.config/i3/config
 
 echo "Reload i3"
 i3-msg reload
