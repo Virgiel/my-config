8fefef144132cf8061dd9e2c17261e1efc87e5b9
Antoine d'Aligny <antoinedaligny@outlook.fr>
2020-12-24 14:14:26 +0100

    Improve config script and start to refactor dotfiles

--- config.js
+++ config.js
@@ -1,16 +1,29 @@
+/**
+ * Config installation tools
+ * Write config files to appropriate places in a cross-platform way
+ */
+
 import { parse } from 'https://deno.land/std@0.82.0/encoding/toml.ts';
 import * as path from 'https://deno.land/std@0.82.0/path/mod.ts';
 import * as color from 'https://deno.land/std@0.82.0/fmt/colors.ts';
+import { ensureFileSync } from 'https://deno.land/std@0.82.0/fs/mod.ts';
 
 const { os } = Deno.build;
+
+if (Deno.args.length == 0) {
+  throw 'You must a config file path as argument';
+}
+
 const rootConfigFile = Deno.args[0];
 const configMetadata = parse(Deno.readTextFileSync(rootConfigFile));
 const fromDir = path.dirname(rootConfigFile);
 
 console.log(color.bold(color.blue(`Start installing ${configMetadata.title}`)));
 
-let osPath;
-let toDir;
+// Get os specific properties
+
+let osPath; // Which path to use
+let toDir; // Which dir to write to
 if (os === 'windows') {
   osPath = 'winpath';
   toDir = 'C:/Users/antoi/AppData';
@@ -33,5 +46,9 @@ for (const item of configMetadata.config) {
   }
 
   const toPath = item[osPath];
-  Deno.writeTextFileSync(path.join(toDir, toPath), configData);
+  const complete = path.join(toDir, toPath);
+  // Create file an parent firs if necessary
+  ensureFileSync(complete);
+  // Write config in config file
+  Deno.writeTextFileSync(complete, configData);
 }
--- tool.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# bottom is on nix packages unstable
-for app in emulsion bottom
-do
- cargo install $app
-done
